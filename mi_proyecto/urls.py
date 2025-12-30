9834638fd393efc2a8537d5d033ad8e721660ad4
brianinfante135-gif <brian.infante135@gmail.com>
2025-12-30 21:15:25 +0000

    Configuración final de estáticos y variables de correo

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
 from django.urls import path
-from mi_proyecto import views
+from contacto import views # Asegúrate de que tu app se llame 'contacto'
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio, name='inicio'),  # Página principal (login)
+    path('', views.inicio, name='inicio'),
     path('interfaz/', views.interfaz, name='interfaz'),
+    path('registro/', views.registro, name='registro'),
+    path('recuperar_password/', views.recuperar_password, name='recuperar_password'),
+    path('reservacion/', views.reservacion, name='reservacion'),
     path('tur1/', views.tur1, name='tur1'),
-     path('registro/', views.registro, name='registro'),
     path('tur2/', views.tur2, name='tur2'),
     path('tur3/', views.tur3, name='tur3'),
     path('tur4/', views.tur4, name='tur4'),
     path('tur5/', views.tur5, name='tur5'),
     path('tur6/', views.tur6, name='tur6'),
-    path('reservacion/', views.reservacion, name='reservacion'),
-    path('recuperar_password/', views.recuperar_password, name='recuperar_password'),
-    
 ]
+
+# Esto sirve para ver las imágenes en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
