365b432a68b56277d5265ce88c06fb68383c6a58
brianinfante135-gif <brian.infante135@gmail.com>
2026-01-03 23:03:31 +0000

    Actualizando dependencias de qrcode y configuracion de settings

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -103,9 +103,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = str(BASE_DIR / 'media')
+import os
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
