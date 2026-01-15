2cb6d4df8ef8552d26928b47e5cbe2966dad8e68
brianinfante135-gif <brian.infante135@gmail.com>
2026-01-14 23:13:57 -0600

    Update URL patterns to include views

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,11 +1,19 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views # El punto (.) es clave aquí
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('contacto.urls')), # Esto es lo que activa todo lo anterior
+    path('', views.inicio, name='inicio'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('interfaz/', views.interfaz, name='interfaz'),
+    path('registro/', views.registro, name='registro'),
+    path('recuperar_password/', views.recuperar_password, name='recuperar_password'),
+    path('reservacion/', views.reservacion, name='reservacion'),
+    path('tur1/', views.tur1, name='tur1'),
+    path('tur2/', views.tur2, name='tur2'),
+    path('tur3/', views.tur3, name='tur3'),
+    path('tur4/', views.tur4, name='tur4'),
+    path('tur5/', views.tur5, name='tur5'),
+    path('tur6/', views.tur6, name='tur6'),
+    path('index/', views.index, name='index'), # Tu CRUD
 ]
-
-
-
 
