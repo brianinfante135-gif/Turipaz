741276d7f5493d5122aaddd321ab12ef1dedd624
brianinfante135-gif <brian.infante135@gmail.com>
2026-01-14 22:47:27 -0600

    Refactor URL patterns to use include for contacto

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from contacto import views
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio, name='inicio'),
-    path('interfaz/', views.interfaz, name='interfaz'),
-    path('registro/', views.registro, name='registro'),
-    path('recuperar_password/', views.recuperar_password, name='recuperar_password'),
-    path('reservacion/', views.reservacion, name='reservacion'),
-    path('tur1/', views.tur1, name='tur1'),
-    path('tur2/', views.tur2, name='tur2'),
-    path('tur3/', views.tur3, name='tur3'),
-    path('tur4/', views.tur4, name='tur4'),
-    path('tur5/', views.tur5, name='tur5'),
-    path('tur6/', views.tur6, name='tur6'),
-    path('index/', views.index, name='index'),
-    
+    path('', include('contacto.urls')), # Esto es lo que activa todo lo anterior
 ]
+
 
 
 
