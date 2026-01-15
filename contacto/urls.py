dd53bdb509155b3faae2a64718f74528e7b94bb1
brianinfante135-gif <brian.infante135@gmail.com>
2026-01-14 22:46:54 -0600

    Update URL patterns to include multiple views

--- contacto/urls.py
+++ contacto/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from . import views
+from . import views # El punto (.) es clave aquí
 
 urlpatterns = [
-    path('', views.contacto, name='contacto'),
-    
+    path('', views.inicio, name='inicio'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('interfaz/', views.interfaz, name='interfaz'),
+    path('registro/', views.registro, name='registro'),
+    path('recuperar_password/', views.recuperar_password, name='recuperar_password'),
+    path('reservacion/', views.reservacion, name='reservacion'),
+    path('tur1/', views.tur1, name='tur1'),
+    path('tur2/', views.tur2, name='tur2'),
+    path('tur3/', views.tur3, name='tur3'),
+    path('tur4/', views.tur4, name='tur4'),
+    path('tur5/', views.tur5, name='tur5'),
+    path('tur6/', views.tur6, name='tur6'),
+    path('index/', views.index, name='index'), # Tu CRUD
 ]
-
-
